16c216604af6bb51ee82ba411e2131c5f541393e
Edward Loughran <edward@data-cubed.co.uk>
2026-01-22 09:28:37 +0000

    added order date

--- adventure_works_demo/models/gold/reporting/vw_fact_sales.sql
+++ adventure_works_demo/models/gold/reporting/vw_fact_sales.sql
@@ -1,21 +1,5 @@
 
 
 SELECT
-    fact_sales.SalesOrderDetailID,
-    fact_sales.SalesOrderID,
-    fact_sales.SalesOrderNumber,
-    fact_sales.PurchaseOrderNumber,
-    fact_sales.CustomerID,
-    fact_sales.ShipToAddressID,
-    fact_sales.BillToAddressID,
-    fact_sales.ShipMethod,
-    fact_sales.SubTotal,
-    fact_sales.TaxAmt,
-    fact_sales.TotalDue,
-    fact_sales.Comment,
-    fact_sales.OrderQty,
-    fact_sales.ProductID,
-    fact_sales.UnitPrice,
-    fact_sales.UnitPriceDiscount,
-    fact_sales.ModifiedDate
+    *
 FROM {{ ref('fact_sales') }} fact_sales
