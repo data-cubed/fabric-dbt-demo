079c824c3926ed82accc2ab09441b23b3710ac89
Kim <kim@data-cubed.co.uk>
2026-07-13 15:21:54 +0100

    Correcting modelled/fact_sales.sql

--- adventure_works_demo/models/gold/modelled/fact_sales.sql
+++ adventure_works_demo/models/gold/modelled/fact_sales.sql
@@ -18,4 +18,4 @@ SELECT
     sales_order_detail.ProductKey,
     sales_order_detail.UnitPrice,
     sales_order_detail.UnitPriceDiscountPct,
-FROM {{ ref('sales_order_header') }} sales_order_header
+FROM {{ ref('sales_order_detail') }} sales_order_detail
